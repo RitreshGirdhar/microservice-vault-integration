79e2ff4f917ee5c2a6999d7c9b3f9cb6e6b64935
ritresh <ritresh.girdhar@loblaw.ca>
2021-05-04 20:24:31 +0530

    created sample application

---
+++ microservice/src/test/java/learning/vault/integration/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.vault.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MicroserviceApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
